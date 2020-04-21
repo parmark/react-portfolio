d202f1a2ad04526b13c2bada9693a7d3a4207720
Kevin Parmar <parmarkevinb@gmail.com>
2020-04-21 18:07:46 -0400

    Edited home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 function Home() {
     return (
-        <div>
-            <p>Home</p>
+        <div className='home'>
+            <p className='bold'>Hello, my name is Kevin Parmar.</p>
+            <p>I am a recent graduate of the UNCC Coding Boot Camp and am pursuring a career in Web Development.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis iusto amalias quis voluptatibus quos autem.</p>
+            <p>
+                <a className='link' href="https://github.com/parmark">Github</a>
+                <a className='link' href="#">LinkedIn</a>
+            </p>
         </div>
     )
 }
