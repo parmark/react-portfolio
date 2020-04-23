89074e2d46c78533196458cdff8f8828bff16442
Kevin Parmar <parmarkevinb@gmail.com>
2020-04-22 23:56:17 -0400

    Changed navigation bar links

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 function Navigation() {
     return (
         <div className='navigation'>
-            <Link className='link right' to='/'><p>About Me</p></Link>
-            <Link className='link left' to='/portfolio'><p>Portfolio</p></Link>
+            <Link className='link right' to='/react-portfolio'><p>About Me</p></Link>
+            <Link className='link left' to='/react-portfolio/portfolio'><p>Portfolio</p></Link>
         </div>
     )
 }
